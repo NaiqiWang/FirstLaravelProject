bc9695f1a859a5165684f8d2f0144a11b3793344
Naiqi Wang <auvsaber@gmail.com>
2015-09-13 11:40:57 -0400

    test commit1

--- Laravel/app/Http/Controllers/ArticleController.php
+++ Laravel/app/Http/Controllers/ArticleController.php
@@ -19,6 +19,7 @@ class ArticleController extends Controller
     public function index()
     {
         $articles = Article::all();
+        $articles = Article::all();
         return view('articles.index', compact('articles'));
     }
 	
